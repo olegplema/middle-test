8b1f5ff4a552114ebf722d7b840c0678c536a892
Oleg Plema <olegplema01@gmail.com>
2024-04-05 10:53:05 +0300

    Write file

--- main.py
+++ main.py
@@ -31,7 +31,24 @@ def find_lines_with_keyword(lines: list[str], keyword: str) -> list[str]:
     return res
 
 
+def write_file(output_file: str, lines: list[str]) -> None:
+    """
+    Write lines to a file.
+
+    Args:
+        output_file (str): The path to the output file.
+        lines (list[str]): A list of strings representing lines of text to be written to the file.
+
+    Returns:
+        None
+    """
+    with open(output_file, 'w') as f:
+        for line in lines:
+            f.write(line)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     file_data = read_file('data.txt')
     result = find_lines_with_keyword(file_data, 'Lorem')
+    write_file('filtered.txt', result)
